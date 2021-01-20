e4d8fc888d455d1a990248df72ec03cce7da7248
ulvienzo <74671555+ulvienzo@users.noreply.github.com>
2021-01-20 20:29:03 +0400

    azərbaycancaya çevrildi

---
+++ script.js
@@ -0,0 +1,31 @@
+const months = [
+    "Yanvar",
+    "Fevral",
+    "Mart",
+    "Aprel",
+    "May",
+    "İyun",
+    "İyul",
+    "Avqust",
+    "Senttybr",
+    "Oktyabr",
+    "Noyabr",
+    "Dekabr",
+  ];
+  const weekdays = [
+    "Bazar",
+    "Bazar ertəsi",
+    "Çərşənbə axşamı",
+    "Çərşənbə",
+    "Cümə axşamı",
+    "Cümə",
+    "Şənbə",
+  ];
+
+
+
+  // Functions
+
+  function getRemainingTime(){
+      
+  }
